c995b81f7dbb63252cf8adee2b216c63b0970f51
Nicholas Bumgarner <nick@inclind.com>
2016-01-23 12:58:35 -0500

    Testing GPS location functionality

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+
+  function whereAreWe() {
+    function distance(lon1, lat1, lon2, lat2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = (lat2 - lat1).toRad();  // Javascript functions in radians
+      var dLon = (lon2 - lon1).toRad();
+      var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+          Math.sin(dLon / 2) * Math.sin(dLon / 2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      var d = R * c; // Distance in km
+      return d;
+    }
+
+    /** Converts numeric degrees to radians */
+    if (typeof(Number.prototype.toRad) === "undefined") {
+      Number.prototype.toRad = function () {
+        return this * Math.PI / 180;
+      }
+    }
+
+    window.navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log(pos);
+      console.log(
+          distance(pos.coords.longitude, pos.coords.latitude, -75.496371, 39.031725)
+      );
+
+
+      var distanceAway = distance(pos.coords.longitude, pos.coords.latitude, -75.496371, 39.031725);
+      if (distanceAway <= .05) {
+        $('body').removeClass('black');
+        $('body').addClass('blue');
+      }
+      else {
+        $('body').addClass('black');
+        $('body').removeClass('blue');
+      }
+    });
+
+  }
+
+  whereAreWe();
+
+  window.setInterval(function(){
+    whereAreWe();
+  }, 5000);
+
+});
+
+// 39.031725, lng:  -75.496371
